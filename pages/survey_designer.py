c91fb32e2cdaf8b71ebd55934e58c075d6ec2830
tjaeni01 <tuckerjaenicke@gmail.com>
2020-09-28 13:13:58 -0400

    adds initial date to absolute schedules

--- pages/survey_designer.py
+++ pages/survey_designer.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from flask import abort, Blueprint, render_template
 
 from authentication.admin_authentication import (authenticate_researcher_study_access,
@@ -32,4 +33,5 @@ def render_edit_survey(survey_id=None):
         weekly_timings=survey.weekly_timings(),
         relative_timings=survey.relative_timings(),
         absolute_timings=survey.absolute_timings(),
+        today=timezone.now().strftime('%Y-%m-%d'),
     )
